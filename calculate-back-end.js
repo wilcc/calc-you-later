bb76f9ee55b6c67dbb35a031775090d899f8d3cb
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-05 12:06:54 -0500

    finish

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,9 +1,28 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function calculate(num,num2,sign){
+  if (sign === '+' || sign === 'plus' || sign === 'added to') {
+    console.log(sign);
+    return +num + +num2
+  }
+  if (sign === '-' || sign === 'minus' || sign === 'subtracted from'){
+    return +num - +num2
+  }
+  if (sign === 'x' || sign === 'times' || sign === 'multiplied by' || sign === 'X'){
+    return +num * +num2
+  }
+  if (sign === '/' || sign === 'divided by'){
+    return +num / +num2
+  }
+  if (sign === '%' || sign === 'mod' || sign === 'modulus'){
+    return +num % +num2
+  }
+  else
+  return "Sorry, that's not a mathematical operation!"
 
+}
 
- 
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
